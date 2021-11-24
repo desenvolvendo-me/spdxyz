b04b87ab5553c7ff4e0f2a021756181573f39485
Marco Castro <marcodotcastro@gmail.com>
2021-11-24 13:49:35 -0300

    build: configure

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,7 +2,7 @@ Sidekiq.configure_client do |config|
   config.redis = if Rails.env.production?
                    { url: ENV['REDIS_URL'], password: ENV['REDIS_PASSWORD'], size: 4, network_timeout: 5 }
                  else
-                   { url: 'redis://0.0.0.0:6379/7', size: 4, network_timeout: 5 }
+                   { url: 'redis://0.0.0.0:63791/7', size: 4, network_timeout: 5 }
                  end
 end
 
@@ -10,6 +10,6 @@ Sidekiq.configure_server do |config|
   config.redis = if Rails.env.production?
                    { url: ENV['REDIS_URL'], password: ENV['REDIS_PASSWORD'], size: 4, network_timeout: 5 }
                  else
-                   { url: 'redis://0.0.0.0:6379/7', size: 4, network_timeout: 5 }
+                   { url: 'redis://0.0.0.0:63791/7', size: 4, network_timeout: 5 }
                  end
 end
