c5a921fbeb9bd021933d15f6531b107e84fa27fa
Marco Castro <marcodotcastro@gmail.com>
2021-11-24 10:40:32 -0300

    feat: read subscribe

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -3,13 +3,13 @@
 class SubscriptionsController < ApplicationController
 
   def read
-    render json: { message: "#{Subscription.count} inscrições na turma SPDv1.7 realizada com sucesso!" }
+    render json: { message: "#{Subscription.count} inscrições na turma SPDv1.7 realizadas com sucesso!" }
   end
 
   def write
     Subscription.create(description: params["name"])
 
-    render json: { message: "#{params["name"]}, sua inscrição na turma SPDv1.7 realizada com sucesso!" }
+    render json: { message: "#{params["name"]}, sua inscrição na turma SPDv1.7 foi realizada com sucesso!" }
   end
 
 end
